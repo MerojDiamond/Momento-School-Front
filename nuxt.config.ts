0872d5e467d1141e7f93aa0946fb3c996e396fe5
Fozilov Meroj <fozilovmerodz@gmail.com>
2025-05-12 23:15:27 +0500

    added many additional packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,40 @@
+import AutoImport from "unplugin-auto-import/vite";
+import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
+import Components from "unplugin-vue-components/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  ssr: false,
+  modules: [
+    "@nuxt/image",
+    "dayjs-nuxt",
+    "@nuxt/icon",
+    "@pinia/nuxt",
+    "pinia-plugin-persistedstate/nuxt",
+    "@vueuse/nuxt",
+    "@nuxtjs/i18n",
+    "@nuxtjs/tailwindcss",
+    "nuxtjs-naive-ui",
+  ],
+  vite: {
+    plugins: [
+      AutoImport({
+        imports: [
+          {
+            "naive-ui": [
+              "useDialog",
+              "useMessage",
+              "useNotification",
+              "useLoadingBar",
+            ],
+          },
+        ],
+      }),
+      Components({
+        resolvers: [NaiveUiResolver()],
+      }),
+    ],
+  },
+});
